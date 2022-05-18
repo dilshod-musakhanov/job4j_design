2c1b7b900cdb841eb944ad389fc7e5e0efcc8cdb
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-05-18 20:52:20 +0400

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/ru/job4j/generics/UserStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.generics;
+
+import java.util.HashMap;
+
+public class UserStore implements Store<User> {
+
+    private final Store<User> store = new MemStore<>();
+
+    @Override
+    public void add(User model) {
+        store.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, User user) {
+        return store.replace(id, user);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return store.delete(id);
+    }
+
+    @Override
+    public User findById(String id) {
+        return store.findById(id);
+    }
+}
