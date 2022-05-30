89d5b198386cec3878931b40d2fd6976fe025ff0
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-05-30 18:59:20 +0400

    3. Переопределить только hashCode [#1003 #307053]

--- src/main/java/ru/job4j/map/UserMap.java
+++ src/main/java/ru/job4j/map/UserMap.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class UserMap {
     public static void main(String[] args) {
-        Calendar calendar = new GregorianCalendar(1980, 8, 8);
-        User user1 = new User("Tom", 1, calendar);
-        User user2 = new User("Alibaba", 7, calendar);
+        Calendar birthday = new GregorianCalendar(1980, 8, 8);
+        User user1 = new User("Tom", 1, birthday);
+        User user2 = new User("Tom", 1, birthday);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
         map.forEach((key, value) -> System.out.println(key + " -> " + value));
     }
+
 }
