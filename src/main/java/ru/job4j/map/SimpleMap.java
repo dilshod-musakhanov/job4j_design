585ab0bfca51ee097d1bc196501485bd7060d3e7
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-06-02 01:20:31 +0400

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,118 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        if ((float) count / (float) capacity >= LOAD_FACTOR) {
+            expand();
+        }
+        int indexKey = indexFor(hash(key.hashCode()));
+        if (table[indexKey] == null) {
+            table[indexKey] = new MapEntry<>(key, value);
+            count++;
+            modCount++;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public V get(K key) {
+        V rsl = null;
+        int indexKey = indexFor(hash(key.hashCode()));
+        if (table[indexKey] != null && Objects.equals(table[indexKey].key, key)) {
+            rsl = table[indexKey].value;
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        boolean rsl = false;
+        int indexKey = indexFor(hash(key.hashCode()));
+        if (table[indexKey] != null && Objects.equals(table[indexKey].key, key)) {
+            table[indexKey].key = null;
+            table[indexKey].value = null;
+            count--;
+            modCount++;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+
+        return new Iterator<K>() {
+
+            private int expectedModCount = modCount;
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < count;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[index++].key;
+            }
+        };
+    }
+
+    private int hash(int hashCode) {
+        return hashCode() ^ (hashCode() >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (table.length - 1);
+    }
+
+    private void expand() {
+        MapEntry<K, V>[] oldTable = table;
+        table = new MapEntry[capacity * 2];
+        count = 0;
+        for (MapEntry<K, V> mapEntry : oldTable) {
+            if (mapEntry != null) {
+                put(mapEntry.key, mapEntry.value);
+            }
+        }
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+}
+
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleMapTest {
+
+    @Test
+    public void whenPutAndGetThenTrue() {
+        Map<String, String> map = new SimpleMap<>();
+        map.put("Hello", "Привет");
+        map.put("GoodBye", "Пока");
+        assertThat("Привет", is(map.get("Hello")));
+    }
+
+    @Test
+    public void whenRemoveThenNull() {
+        Map<String, Integer> map = new SimpleMap<>();
+        map.put("TV", 100);
+        map.put("Phone", 200);
+        map.remove("TV");
+        assertNull(map.get("TV"));
+    }
+
+    @Test
+    public void whenMoreThan8ThenExpand() {
+        Map<String, Integer> map = new SimpleMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        assertThat(8, is(map.get("eight")));
+    }
+
+}
