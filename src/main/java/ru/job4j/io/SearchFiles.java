aeaeb218805d230bd6aaa9aeaf046d61bab7489f
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-06-12 00:21:14 +0400

    Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private final Predicate<Path> condition;
+    private final List<Path> pathList = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path path = Paths.get(file.getFileName().toString());
+        if (condition.test(path)) {
+            pathList.add(path);
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return pathList;
+    }
+}
