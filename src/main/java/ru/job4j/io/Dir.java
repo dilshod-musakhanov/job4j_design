c2d95f7169573022b58e621b39c2b74697d534b2
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-06-14 00:22:34 +0400

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -2,10 +2,11 @@ package ru.job4j.io;
 
 import java.io.File;
 
+import static ru.job4j.io.Search.validateArgs;
+
 public class Dir {
     public static void main(String[] args) {
-        Search s = new Search();
-        s.validateArgs(args);
+        validateArgs(args);
         File file = new File(args[0]);
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,24 +10,20 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Search s = new Search();
-        if (s.validateArgs(args)) {
-            Path start = Paths.get(args[0]);
-            search(start, f -> f.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-        }
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, f -> f.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
-    public boolean validateArgs(String[] args) {
+    public static void validateArgs(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Please provide both parameters");
         }
-        if (args[0] == null) {
+        if (!Paths.get(args[0]).toFile().exists()) {
             throw new IllegalArgumentException("Please provide a valid directory");
         }
         if (args[1].startsWith(".")) {
             throw new IllegalArgumentException("Please provide a valid file extension");
-        } else {
-            return true;
         }
     }
 
