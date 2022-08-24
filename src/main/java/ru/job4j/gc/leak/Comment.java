773062f03ec706844ffab056912a013eaf498f69
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-08-24 09:41:13 +0400

    Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/Comment.java
@@ -0,0 +1,26 @@
+package ru.job4j.gc.leak;
+
+public class Comment {
+
+    private String text;
+
+    private User user;
+
+    public Comment(String text, User user) {
+        this.text = text;
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+}
