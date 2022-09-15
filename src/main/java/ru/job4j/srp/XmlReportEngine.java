0be1cb8bf2adb7f1ba7e14d2f6a651667a5d753f
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-09-15 09:54:41 +0400

    Поддержка форматом XML, JSON в генераторе отчетов. [#851 #342781]

--- src/main/java/ru/job4j/srp/XmlReportEngine.java
+++ src/main/java/ru/job4j/srp/XmlReportEngine.java
@@ -4,25 +4,15 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.StringWriter;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class XmlReportEngine implements Report {
-
     private Store store;
-
-    private StringBuilder result = new StringBuilder();
+    private JAXBContext context;
+    private Marshaller marshaller;
 
     public XmlReportEngine(Store store) {
         this.store = store;
-    }
-
-    @Override
-    public String generate(Predicate<Employee> filter) {
-        List<Employee> temp = store.findBy(filter);
-        JAXBContext context;
-        Marshaller marshaller;
-        String result;
         try {
             context = JAXBContext.newInstance(Employees.class);
             marshaller = context.createMarshaller();
@@ -30,8 +20,13 @@ public class XmlReportEngine implements Report {
         } catch (JAXBException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        String result;
         try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(new Employees(temp), writer);
+            marshaller.marshal(new Employees(store.findBy(filter)), writer);
             result = writer.getBuffer().toString();
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
