e66227bbc5d840f8c3affa01a5ea970df04a9710
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-09-19 21:27:55 +0400

    Парковка машин [#853]

---
+++ src/main/java/ru/job4j/lsp/parking/Parking.java
@@ -0,0 +1,6 @@
+package ru.job4j.lsp.parking;
+
+public interface Parking {
+    int createParking();
+    boolean getParking(Vehicle vehicle);
+}
---
+++ src/main/java/ru/job4j/lsp/parking/ParkingOperator.java
@@ -0,0 +1,16 @@
+package ru.job4j.lsp.parking;
+
+public class ParkingOperator {
+
+    public ParkingOperator(Parking parking) {
+    }
+
+    public boolean checkFreeSpace(Vehicle vehicle) {
+        return true;
+    }
+
+    public boolean allowParking(Vehicle vehicle) {
+        return true;
+    }
+
+}
